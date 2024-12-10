42de68f5883c19de0e5c859fdac1a8fb32534684
VoidCrowned <VoidCrowned@fastmail.fm>
2024-12-10 19:39:42 +0100

    Reworked a few lines.

--- arch_install.sh
+++ arch_install.sh
@@ -8,10 +8,10 @@ fbold='\033[1m'
 ## Success?
 success() {
     if [ $? -eq 0 ]; then
-        echo $suc
+        echo -e $suc
         exit 0
     else
-        echo $err
+        echo -e $err
         exit 1
     fi
 }
@@ -23,51 +23,115 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-# Update reflector for better mirrors
+## Pacman mirrorlist update
+task="Pacman mirrorlist update"
+suc="Successfully executed '$task'."
+err="Failed to execute '$task'."
+echo -e "Updating pacman mirrolist..."
 reflector -c EU -p https --age 6 --fastest 5 --sort rate --save /etc/pacman.d/mirrorlist
-echo "Updated mirrors."
+success
+
+## Pacman settigs
+suc="Updated pacman.conf..."
+err="Failed to update pacman.conf."
+echo -e "Updatiing pacman.conf."
 cat pacman_settings >> /etc/pacman.conf
-echo "Appended pacman_settings to pacman.conf."
+success
 
-# Installing base system
-echo "Installing system base & CPU security patches."
-pacstrap -K /mnt base linux linux-firmware intel-ucode 
 
-# fstab & chroot
+## Pacman db update
+suc="Updated pacman db."
+err="Failed to update pacman."
+echo -e "Updating pacman..."
+pacman -Syu --noconfirm
+success
+
+
+## Pacstrap
+pkglist=("base" "linux" "linux-firmware" "intel-ucode")
+suc="Done!"
+err="Oops!"
+echo -e "Pacstrapping $pkglist."
+pacstrap -K $pkglist
+success
+
+
+## fstab
+suc="Generated fstab successfully."
+err="Failed to generate fstab."
+echo -e "Generating fstab..."
 #genfstab -U /mnt >> /mnt/etc/fstab
-#arch-chroot /mnt
+success
 
-# Update pacman files
-echo "Updating pacman files."
-#pacman -Syu
+## chroot
+suc="Successfully chrooted."
+err="Failed to execute chroot."
+echo -e "Chrooting..."
+arch-chroot /mnt
+success
 
-# Date & Time
-echo "Setting time to 'Europe/Berlin'."
+## Update pacman files
+suc="Updated pacman mirrorlist..."
+err="Failed to update pacman mirrorlist."
+echo -e "Updating pacman mirrolist."
+reflector -c EU -p https --age 6 --fastest 5 --sort rate --save /etc/pacman.d/mirrorlist
+success
+
+## Pacman settigs
+suc="Updated pacman.conf..."
+err="Failed to update pacman.conf."
+echo -e "Updatiing pacman.conf."
+cat pacman_settings >> /etc/pacman.conf
+success
+
+
+## Pacman db update
+#suc="Updated pacman db."
+#err="Failed to update pacman."
+#echo -e "Updating pacman..."
+#pacman -Syu --noconfirm
+#success
+
+## Date & time
+suc="Date & time set."
+err="Failed to set date & time."
+echo -e "Setting date & time..."
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+success
 
-echo "Syncronising hw clock with sys clock."
+## Clock sync
+suc="Sysclock synced to hwclock."
+err="Failed to sync sysclock to hwclock."
+echo -e "Syncing sysclock to hwclock..."
 hwclock --systohc
+success
+
 
 ## Localisation
+suc=""
+err=""
 # Edit '/etc/locale.gen', uncomment langs
 # Then run 'locale-gen'
 ## Create locale.conf
 #mv /etc/locale.conf /etc/locale.conf.bak
 #mv /etc/locale.conf.new /etc/locale.conf
 
+
 ## Persistent console settings
+# KBM
 suc="Console kbm set successfully."
 err="Failed to set console kbm."
-echo "Creating backup of '/etc/vconsole.conf'."
+echo -e "Creating backup of '/etc/vconsole.conf'."
 cp /etc/vconsole.conf /etc/vconsole.conf.bak
-echo "Setting console kbm."
-echo "KEYMAP=dvorak" >> /etc/vconsole.conf
+echo -e "Setting console kbm."
+echo -e "KEYMAP=dvorak" >> /etc/vconsole.conf
 success
 
+# Font
 suc="Console font set successfully."
 err="Failed to set console font."
-echo "Setting console font."
-echo "FONT=LAT2-terminus16" >> /etc/vconsole.conf
+echo -e "Setting console font."
+echo -e "FONT=Lat2-Terminus16" >> /etc/vconsole.conf
 success
 
 # Network config
@@ -83,5 +147,5 @@ success
 
 # Reboot
 
-echo "All scripts executed successfully!"
+echo -e "All scripts executed successfully!"
 exit 0
